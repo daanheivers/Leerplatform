3ccdf3e5364b97cc1ca313b6c82fcef619062301
daanheivers <daan.heivers@student.ucll.be>
2022-03-06 20:46:37 +0100

    Project opnieuw opgestart met MVC

---
+++ Leerplatform/Leerplatform/Leerplatform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Leerplatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
